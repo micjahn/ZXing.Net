7a9178697f33ee1ceee6c9cdf7e50e6451aac346
micjahn <mikej06@hotmail.com>
2012-01-02 21:31:08 +0000

    tuning assembly attributes, added signing of the assemblies
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C72110

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,20 +1,33 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-// TODO: Review the values of the assembly attributes
-
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+#if NET20
+[assembly: AssemblyTitle("zxing.net for .net 2.0")]
+#endif
+#if NET40
+[assembly: AssemblyTitle("zxing.net for .net 4.0")]
+#endif
+#if SILVERLIGHT4
+[assembly: AssemblyTitle("zxing.net for silverlight 4")]
+#endif
+#if WINDOWS_PHONE70
+[assembly: AssemblyTitle("zxing.net for windows phone 7.0")]
+#endif
+#if WINDOWS_PHONE71
+[assembly: AssemblyTitle("zxing.net for windows phone 7.1")]
+#endif
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net")]
+[assembly: AssemblyCompany("ZXing.Net Developement")]
+[assembly: AssemblyProduct("ZXing.Net")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
+[assembly: Guid("ECE3AB74-9DD1-4CFB-9D48-FCBFB30E06D6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -26,37 +39,5 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
-
-//
-// In order to sign your assembly you must specify a key to use. Refer to the 
-// Microsoft .NET Framework documentation for more information on assembly signing.
-//
-// Use the attributes below to control which key is used for signing. 
-//
-// Notes: 
-//   (*) If no key is specified, the assembly is not signed.
-//   (*) KeyName refers to a key that has been installed in the Crypto Service
-//       Provider (CSP) on your machine. KeyFile refers to a file which contains
-//       a key.
-//   (*) If the KeyFile and the KeyName values are both specified, the 
-//       following processing occurs:
-//       (1) If the KeyName can be found in the CSP, that key is used.
-//       (2) If the KeyName does not exist and the KeyFile does exist, the key 
-//           in the KeyFile is installed into the CSP and used.
-//   (*) In order to create a KeyFile, you can use the sn.exe (Strong Name) utility.
-//       When specifying the KeyFile, the location of the KeyFile should be
-//       relative to the project output directory which is
-//       %Project Directory%\obj\<configuration>. For example, if your KeyFile is
-//       located in the project directory, you would specify the AssemblyKeyFile 
-//       attribute as [assembly: AssemblyKeyFile("..\..\mykey.snk")]
-//   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
-//       documentation for more information on this.
-//
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
-[assembly: AssemblyKeyName("")]
-
-
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
