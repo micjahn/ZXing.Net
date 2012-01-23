b8fb03e804e11a13576429187b2348382bd784ca
micjahn <mikej06@hotmail.com>
2012-01-23 21:51:47 +0000

    FalsePositives2BlackBox, FalsePositivesBlackBox, PartialBlackBox and UnsupportedBlackBox tests added
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C72508

---
+++ Source/test/src/negative/FalsePositives2BlackBoxTestCase.cs
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using com.google.zxing.common;
+
+namespace com.google.zxing.negative
+{
+   /// <summary>
+   /// Additional random images with high contrast patterns which should not find any barcodes.
+   ///
+   /// <author>dswitkin@google.com (Daniel Switkin)</author>
+   /// </summary>
+   public sealed class FalsePositives2BlackBoxTestCase : AbstractNegativeBlackBoxTestCase
+   {
+      public FalsePositives2BlackBoxTestCase()
+         : base("test/data/blackbox/falsepositives-2")
+      {
+         addTest(5, 0.0f);
+         addTest(5, 90.0f);
+         addTest(5, 180.0f);
+         addTest(5, 270.0f);
+      }
+   }
+}
