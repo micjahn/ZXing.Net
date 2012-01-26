f85d1853fc307b8364443bb560862fa756fe2073
micjahn <mikej06@hotmail.com>
2012-01-26 21:27:51 +0000

    port rev. 2116: Count misreads in the test framework and track. Retune slightly on this basis to pick up a few more images
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C72600

--- Source/lib/oned/UPCEANReader.cs
+++ Source/lib/oned/UPCEANReader.cs
@@ -36,7 +36,7 @@ namespace com.google.zxing.oned
       // These two values are critical for determining how permissive the decoding will be.
       // We've arrived at these values through a lot of trial and error. Setting them any higher
       // lets false positives creep in quickly.
-      private static int MAX_AVG_VARIANCE = (int)(PATTERN_MATCH_RESULT_SCALE_FACTOR * 0.42f);
+      private static int MAX_AVG_VARIANCE = (int)(PATTERN_MATCH_RESULT_SCALE_FACTOR * 0.48f);
       private static int MAX_INDIVIDUAL_VARIANCE = (int)(PATTERN_MATCH_RESULT_SCALE_FACTOR * 0.7f);
 
       /// <summary>
