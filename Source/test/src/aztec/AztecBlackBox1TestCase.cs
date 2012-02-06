a32fa6a5d86c7611bed5c81f3ad8b961c99f0e1d
micjahn <mikej06@hotmail.com>
2012-02-06 20:22:12 +0000

    port rev. 2160: Test from issue 1112
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C72848

--- Source/test/src/aztec/AztecBlackBox1TestCase.cs
+++ Source/test/src/aztec/AztecBlackBox1TestCase.cs
@@ -26,10 +26,10 @@ namespace com.google.zxing.aztec
       public AztecBlackBox1TestCase()
          : base("test/data/blackbox/aztec-1", new AztecReader(), BarcodeFormat.AZTEC)
       {
-         addTest(9, 9, 0.0f);
-         addTest(9, 9, 90.0f);
-         addTest(9, 9, 180.0f);
-         addTest(9, 9, 270.0f);
+         addTest(11, 11, 0.0f);
+         addTest(11, 11, 90.0f);
+         addTest(11, 11, 180.0f);
+         addTest(11, 11, 270.0f);
       }
    }
 }
