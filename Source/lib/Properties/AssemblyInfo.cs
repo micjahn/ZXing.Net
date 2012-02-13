1ef5e63ac48488c9a05e6a6c6ce7d5ed6b2efa23
micjahn <mikej06@hotmail.com>
2012-02-13 19:41:47 +0000

    Preparing release 0.2.0.0
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C73024

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 #if WINDOWS_PHONE71
 [assembly: AssemblyTitle("zxing.net for windows phone 7.1")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing rev. 2160)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing rev. 2181)")]
 [assembly: AssemblyCompany("ZXing.Net Developement")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
--- nuget-push.cmd
+++ nuget-push.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
 SET Id=ZXing.Net
-SET VERSION=0.1.0.0
+SET VERSION=0.2.0.0
 
 3rdParty\nuget\nuget push Build\nuget\%ID%.%VERSION%.nupkg
