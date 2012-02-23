a41ca22ef8b76df9108f4fd6589bcc2df88cb816
micjahn <mikej06@hotmail.com>
2012-02-23 20:21:36 +0000

    one namespace left
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C73308

--- Source/lib/RGBLuminanceSource.cs
+++ Source/lib/RGBLuminanceSource.cs
@@ -4,84 +4,84 @@ using System.Drawing.Imaging;
 using System.Drawing;
 #endif
 
-using ZXing;
-
-public class RGBLuminanceSource : LuminanceSource
+namespace ZXing
 {
+   public class RGBLuminanceSource : LuminanceSource
+   {
 
-   private sbyte[] luminances;
-   private bool isRotated = false;
-   private bool __isRegionSelect = false;
+      private sbyte[] luminances;
+      private bool isRotated = false;
+      private bool __isRegionSelect = false;
 #if SILVERLIGHT
    private System.Windows.Rect __Region;
 #else
-   private Rectangle __Region;
+      private Rectangle __Region;
 #endif
 
-   override public int Height
-   {
-      get
+      override public int Height
       {
-         if (!isRotated)
-            return __height;
-         else
-            return __width;
-      }
-
-   }
-   override public int Width
-   {
-      get
-      {
-         if (!isRotated)
-            return __width;
-         else
-            return __height;
-      }
-
-   }
-   private int __height;
-   private int __width;
-
-   public RGBLuminanceSource(byte[] d, int W, int H)
-      : base(W, H)
-   {
-      __width = W;
-      __height = H;
-      int width = W;
-      int height = H;
-      // In order to measure pure decoding speed, we convert the entire image to a greyscale array
-      // up front, which is the same as the Y channel of the YUVLuminanceSource in the real app.
-      luminances = new sbyte[width * height];
-      for (int y = 0; y < height; y++)
-      {
-         int offset = y * width;
-         for (int x = 0; x < width; x++)
+         get
          {
-            int r = d[offset * 3 + x * 3];
-            int g = d[offset * 3 + x * 3 + 1];
-            int b = d[offset * 3 + x * 3 + 2];
-            if (r == g && g == b)
-            {
-               // Image is already greyscale, so pick any channel.
-               luminances[offset + x] = (sbyte)r;
-            }
+            if (!isRotated)
+               return __height;
             else
+               return __width;
+         }
+
+      }
+      override public int Width
+      {
+         get
+         {
+            if (!isRotated)
+               return __width;
+            else
+               return __height;
+         }
+
+      }
+      private int __height;
+      private int __width;
+
+      public RGBLuminanceSource(byte[] d, int W, int H)
+         : base(W, H)
+      {
+         __width = W;
+         __height = H;
+         int width = W;
+         int height = H;
+         // In order to measure pure decoding speed, we convert the entire image to a greyscale array
+         // up front, which is the same as the Y channel of the YUVLuminanceSource in the real app.
+         luminances = new sbyte[width * height];
+         for (int y = 0; y < height; y++)
+         {
+            int offset = y * width;
+            for (int x = 0; x < width; x++)
             {
-               // Calculate luminance cheaply, favoring green.
-               luminances[offset + x] = (sbyte)((r + g + g + b) >> 2);
+               int r = d[offset * 3 + x * 3];
+               int g = d[offset * 3 + x * 3 + 1];
+               int b = d[offset * 3 + x * 3 + 2];
+               if (r == g && g == b)
+               {
+                  // Image is already greyscale, so pick any channel.
+                  luminances[offset + x] = (sbyte)r;
+               }
+               else
+               {
+                  // Calculate luminance cheaply, favoring green.
+                  luminances[offset + x] = (sbyte)((r + g + g + b) >> 2);
+               }
             }
          }
       }
-   }
-   public RGBLuminanceSource(byte[] d, int W, int H, bool Is8Bit)
-      : base(W, H)
-   {
-      __width = W;
-      __height = H;
-      luminances = new sbyte[W * H];
-      Buffer.BlockCopy(d, 0, luminances, 0, W * H);
-   }
+      public RGBLuminanceSource(byte[] d, int W, int H, bool Is8Bit)
+         : base(W, H)
+      {
+         __width = W;
+         __height = H;
+         luminances = new sbyte[W * H];
+         Buffer.BlockCopy(d, 0, luminances, 0, W * H);
+      }
 
 #if SILVERLIGHT
 
@@ -124,84 +124,85 @@ public class RGBLuminanceSource : LuminanceSource
 
 #else
 
-   public RGBLuminanceSource(byte[] d, int W, int H, bool Is8Bit, Rectangle Region)
-      : base(W, H)
-   {
-      __width = Region.Width;
-      __height = Region.Height;
-      __Region = Region;
-      __isRegionSelect = true;
-      //luminances = Red.Imaging.Filters.CropArea(d, W, H, Region);
-   }
-
-   public RGBLuminanceSource(Bitmap d, int W, int H)
-      : base(W, H)
-   {
-      int width = __width = W;
-      int height = __height = H;
-      // In order to measure pure decoding speed, we convert the entire image to a greyscale array
-      // up front, which is the same as the Y channel of the YUVLuminanceSource in the real app.
-      luminances = new sbyte[width * height];
-      //if (format == PixelFormat.Format8bppIndexed)
+      public RGBLuminanceSource(byte[] d, int W, int H, bool Is8Bit, Rectangle Region)
+         : base(W, H)
       {
-         Color c;
-         for (int y = 0; y < height; y++)
+         __width = Region.Width;
+         __height = Region.Height;
+         __Region = Region;
+         __isRegionSelect = true;
+         //luminances = Red.Imaging.Filters.CropArea(d, W, H, Region);
+      }
+
+      public RGBLuminanceSource(Bitmap d, int W, int H)
+         : base(W, H)
+      {
+         int width = __width = W;
+         int height = __height = H;
+         // In order to measure pure decoding speed, we convert the entire image to a greyscale array
+         // up front, which is the same as the Y channel of the YUVLuminanceSource in the real app.
+         luminances = new sbyte[width * height];
+         //if (format == PixelFormat.Format8bppIndexed)
          {
-            int offset = y * width;
-            for (int x = 0; x < width; x++)
+            Color c;
+            for (int y = 0; y < height; y++)
             {
-               c = d.GetPixel(x, y);
-               luminances[offset + x] = (sbyte)(((int)c.R) << 16 | ((int)c.G) << 8 | ((int)c.B));
+               int offset = y * width;
+               for (int x = 0; x < width; x++)
+               {
+                  c = d.GetPixel(x, y);
+                  luminances[offset + x] = (sbyte)(((int)c.R) << 16 | ((int)c.G) << 8 | ((int)c.B));
+               }
             }
          }
       }
-   }
 
 #endif
 
-   override public sbyte[] getRow(int y, sbyte[] row)
-   {
-      if (isRotated == false)
+      override public sbyte[] getRow(int y, sbyte[] row)
       {
-         int width = Width;
-         if (row == null || row.Length < width)
+         if (isRotated == false)
          {
-            row = new sbyte[width];
+            int width = Width;
+            if (row == null || row.Length < width)
+            {
+               row = new sbyte[width];
+            }
+            for (int i = 0; i < width; i++)
+               row[i] = luminances[y * width + i];
+            //System.arraycopy(luminances, y * width, row, 0, width);
+            return row;
          }
-         for (int i = 0; i < width; i++)
-            row[i] = luminances[y * width + i];
-         //System.arraycopy(luminances, y * width, row, 0, width);
-         return row;
+         else
+         {
+            int width = __width;
+            int height = __height;
+            if (row == null || row.Length < height)
+            {
+               row = new sbyte[height];
+            }
+            for (int i = 0; i < height; i++)
+               row[i] = luminances[i * width + y];
+            //System.arraycopy(luminances, y * width, row, 0, width);
+            return row;
+         }
       }
-      else
+      public override sbyte[] Matrix
       {
-         int width = __width;
-         int height = __height;
-         if (row == null || row.Length < height)
-         {
-            row = new sbyte[height];
-         }
-         for (int i = 0; i < height; i++)
-            row[i] = luminances[i * width + y];
-         //System.arraycopy(luminances, y * width, row, 0, width);
-         return row;
+         get { return luminances; }
       }
-   }
-   public override sbyte[] Matrix
-   {
-      get { return luminances; }
-   }
 
-   public override LuminanceSource rotateCounterClockwise()
-   {
-      isRotated = true;
-      return this;
-   }
-   public override bool RotateSupported
-   {
-      get
+      public override LuminanceSource rotateCounterClockwise()
       {
-         return true;
+         isRotated = true;
+         return this;
+      }
+      public override bool RotateSupported
+      {
+         get
+         {
+            return true;
+         }
       }
    }
 }
