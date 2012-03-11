efa8e7833f71dfe014cbd0dc3a11e7d1a563a245
micjahn <mikej06@hotmail.com>
2012-03-11 20:37:53 +0000

    great speed improvement for RGBLuminanceSource (.net 2.0 and .net 4.0) fast, faster, LockBits :)
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C74344

--- Source/lib/RGBLuminanceSource.cs
+++ Source/lib/RGBLuminanceSource.cs
@@ -2,6 +2,8 @@
 #if !SILVERLIGHT
 using System.Drawing.Imaging;
 using System.Drawing;
+using System.Runtime.InteropServices;
+
 #endif
 
 namespace ZXing
@@ -142,18 +144,91 @@ namespace ZXing
          // In order to measure pure decoding speed, we convert the entire image to a greyscale array
          // up front, which is the same as the Y channel of the YUVLuminanceSource in the real app.
          luminances = new sbyte[width * height];
-         //if (format == PixelFormat.Format8bppIndexed)
+          
+         // The underlying raster of image consists of bytes with the luminance values
+         var data = d.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, d.PixelFormat);
+         try
          {
-            Color c;
-            for (int y = 0; y < height; y++)
+            var stride = Math.Abs(data.Stride);
+            var pixelWidth = stride / width;
+
+            if (pixelWidth == 2 || pixelWidth > 4)
             {
-               int offset = y * width;
-               for (int x = 0; x < width; x++)
+               // old slow way for unsupported bit depth
+               Color c;
+               for (int y = 0; y < height; y++)
                {
-                  c = d.GetPixel(x, y);
-                  luminances[offset + x] = (sbyte) (0.3*c.R + 0.59*c.G + 0.11*c.B);
+                  int offset = y*width;
+                  for (int x = 0; x < width; x++)
+                  {
+                     c = d.GetPixel(x, y);
+                     luminances[offset + x] = (sbyte) (0.3*c.R + 0.59*c.G + 0.11*c.B);
+                  }
                }
             }
+            else
+            {
+               var strideStep = data.Stride;
+               var buffer = new byte[stride];
+               var ptrInBitmap = data.Scan0;
+
+               // prepare palette for 1 and 8 bit indexed bitmaps
+               var luminancePalette = new sbyte[d.Palette.Entries.Length];
+               for (var index = 0; index < d.Palette.Entries.Length; index++)
+               {
+                  var color = d.Palette.Entries[index];
+                  luminancePalette[index] = (sbyte) (0.3*color.R +
+                                                     0.59*color.G +
+                                                     0.11*color.B);
+               }
+
+               for (int y = 0; y < height; y++)
+               {
+                  // copy a scanline not the whole bitmap because of memory usage
+                  Marshal.Copy(ptrInBitmap, buffer, 0, stride);
+#if NET40
+                  ptrInBitmap = IntPtr.Add(ptrInBitmap, strideStep);
+#else
+                  ptrInBitmap = new IntPtr(ptrInBitmap.ToInt64() + strideStep);
+#endif
+                  var offset = y*width;
+                  switch (pixelWidth)
+                  {
+                     case 0:
+                        for (int x = 0; x*8 < width; x++)
+                        {
+                           for (int subX = 0; subX < 8 && 8*x + subX < width; subX++)
+                           {
+                              var index = (buffer[x] >> (7 - subX)) & 1;
+                              luminances[offset + 8*x + subX] = luminancePalette[index];
+                           }
+                        }
+                        break;
+                     case 1:
+                        for (int x = 0; x < width; x++)
+                        {
+                           luminances[offset + x] = luminancePalette[buffer[x]];
+                        }
+                        break;
+                     case 3:
+                     case 4:
+                        for (int x = 0; x < width; x++)
+                        {
+                           var luminance = (sbyte) (0.3*buffer[x*pixelWidth] +
+                                                    0.59*buffer[x*pixelWidth + 1] +
+                                                    0.11*buffer[x*pixelWidth + 2]);
+                           luminances[offset + x] = luminance;
+                        }
+                        break;
+                     default:
+                        throw new NotSupportedException();
+                  }
+               }
+            }
+         }
+         finally
+         {
+            d.UnlockBits(data);
          }
       }
 
