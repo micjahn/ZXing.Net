89d94d6d5e6cb853a604f4eec25ec9ccae1c14a7
micjahn <mikej06@hotmail.com>
2012-06-07 20:52:52 +0000

    port rev. 2278: Forgot tests for PDF417 EC
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C78649

---
+++ Source/test/src/pdf417/decoder/ec/AbstractErrorCorrectionTestCase.cs
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2012 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+
+using NUnit.Framework;
+
+/// <summary>
+/// @author Sean Owen
+/// </summary>
+public abstract class AbstractErrorCorrectionTestCase
+{
+   public static void corrupt(int[] received, int howMany, Random random)
+   {
+      var corrupted = new System.Collections.BitArray(received.Length);
+      // BitSet corrupted = new BitSet(received.Length);
+      for (int j = 0; j < howMany; j++)
+      {
+         int location = random.Next(received.Length);
+         if (corrupted[location])
+         {
+            j--;
+         }
+         else
+         {
+            corrupted[location] = true;
+            received[location] = random.Next(929);
+         }
+      }
+   }
+
+   public static int[] erase(int[] received, int howMany, Random random)
+   {
+      var erased = new System.Collections.BitArray(received.Length);
+      // BitSet erased = new BitSet(received.Length);
+      int[] erasures = new int[howMany];
+      int erasureOffset = 0;
+      for (int j = 0; j < howMany; j++)
+      {
+         int location = random.Next(received.Length);
+         if (erased[location])
+         {
+            j--;
+         }
+         else
+         {
+            erased[location] = true;
+            received[location] = 0;
+            erasures[erasureOffset++] = location;
+         }
+      }
+      return erasures;
+   }
+
+   public static Random getRandom()
+   {
+      return new Random((int)DateTime.Now.Ticks);
+      // return new SecureRandom(new byte[] { (byte)0xDE, (byte)0xAD, (byte)0xBE, (byte)0xEF });
+   }
+
+   public static void assertArraysEqual(int[] expected,
+                                 int expectedOffset,
+                                 int[] actual,
+                                 int actualOffset,
+                                 int length)
+   {
+      for (int i = 0; i < length; i++)
+      {
+         Assert.AreEqual(expected[expectedOffset + i], actual[actualOffset + i]);
+      }
+   }
+}
---
+++ Source/test/src/pdf417/decoder/ec/ErrorCorrectionTestCase.cs
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2012 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using NUnit.Framework;
+
+using ZXing.PDF417.Internal.EC;
+
+/// <summary>
+/// @author Sean Owen
+/// </summary>
+public sealed class ErrorCorrectionTestCase : AbstractErrorCorrectionTestCase
+{
+
+   /** See ISO 15438, Annex Q */
+   //private static final int[] PDF417_TEST =
+   //    { 5, 453, 178, 121, 239 };
+   //private static final int[] PDF417_TEST_WITH_EC =
+   //    { 5, 453, 178, 121, 239, 452, 327, 657, 619 };
+
+   private static readonly int[] PDF417_TEST =
+      { 48,901, 56,141,627,856,330, 69,244,900,
+       852,169,843,895,852,895,913,154,845,778,
+       387, 89,869,901,219,474,543,650,169,201,
+         9,160, 35, 70,900,900,900,900,900,900,
+       900,900,900,900,900,900,900,900};
+   private static readonly int[] PDF417_TEST_WITH_EC =
+      { 48,901, 56,141,627,856,330, 69,244,900,
+       852,169,843,895,852,895,913,154,845,778,
+       387, 89,869,901,219,474,543,650,169,201,
+         9,160, 35, 70,900,900,900,900,900,900,
+       900,900,900,900,900,900,900,900,769,843,
+       591,910,605,206,706,917,371,469, 79,718,
+        47,777,249,262,193,620,597,477,450,806,
+       908,309,153,871,686,838,185,674, 68,679,
+       691,794,497,479,234,250,496, 43,347,582,
+       882,536,322,317,273,194,917,237,420,859,
+       340,115,222,808,866,836,417,121,833,459,
+        64,159};
+   private static readonly int ECC_BYTES = PDF417_TEST_WITH_EC.Length - PDF417_TEST.Length;
+   // Example is EC level 1 (s=1). The number of erasures (l) and substitutions (f) must obey:
+   // l + 2f <= 2^(s+1) - 3
+   private const int EC_LEVEL = 5;
+   private const int ERROR_LIMIT = (1 << (EC_LEVEL + 1)) - 3;
+   private const int MAX_ERRORS = ERROR_LIMIT / 2;
+   //private static final int MAX_ERASURES = ERROR_LIMIT;
+
+   private readonly ErrorCorrection ec = new ErrorCorrection();
+
+   [Test]
+   public void testNoError()
+   {
+      int[] received = (int[])PDF417_TEST_WITH_EC.Clone();
+      // no errors
+      checkDecode(received);
+   }
+
+   [Test]
+   public void testOneError()
+   {
+      Random random = getRandom();
+      for (int i = 0; i < PDF417_TEST_WITH_EC.Length; i++)
+      {
+         int[] received = (int[])PDF417_TEST_WITH_EC.Clone();
+         received[i] = random.Next(256);
+         checkDecode(received);
+      }
+   }
+
+   [Test]
+   public void testMaxErrors()
+   {
+      Random random = getRandom();
+      foreach (int test in PDF417_TEST)
+      { // # iterations is kind of arbitrary
+         int[] received = (int[])PDF417_TEST_WITH_EC.Clone();
+         corrupt(received, MAX_ERRORS, random);
+         checkDecode(received);
+      }
+   }
+
+   [Test]
+   public void testTooManyErrors()
+   {
+      int[] received = (int[])PDF417_TEST_WITH_EC.Clone();
+      Random random = getRandom();
+      corrupt(received, MAX_ERRORS + 3, random); // +3 since the algo can actually correct 2 more than it should here
+      Assert.IsFalse(checkDecode(received));
+   }
+
+   /*
+   @Test
+   public void testMaxErasures() throws ChecksumException {
+     Random random = getRandom();
+     for (int test : PDF417_TEST) { // # iterations is kind of arbitrary
+       int[] received = PDF417_TEST_WITH_EC.clone();
+       int[] erasures = erase(received, MAX_ERASURES, random);
+       checkDecode(received, erasures);
+     }
+   }
+
+   @Test
+   public void testTooManyErasures() {
+     Random random = getRandom();
+     int[] received = PDF417_TEST_WITH_EC.clone();
+     int[] erasures = erase(received, MAX_ERASURES + 1, random);
+     try {
+       checkDecode(received, erasures);
+       fail("Should not have decoded");
+     } catch (ChecksumException ce) {
+       // good
+     }
+   }
+    */
+
+   private bool checkDecode(int[] received)
+   {
+      if (!ec.decode(received, ECC_BYTES))
+         return false;
+      
+      for (int i = 0; i < PDF417_TEST.Length; i++)
+      {
+         if (received[i] != PDF417_TEST[i])
+            return false;
+      }
+
+      return true;
+   }
+}
