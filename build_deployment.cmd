88c5c5416d6be98097e8437992de671ac1f01b96
micjahn <mikej06@hotmail.com>
2012-06-09 12:16:17 +0000

    source archive completed
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C78744

--- build_deployment.cmd
+++ build_deployment.cmd
@@ -11,7 +11,7 @@ SET FILENAME_DEMO_BINARY=%DEPLOYMENT_DIR%\ZXing.Net.DemoClients.%VERSION%.zip
 SET FILENAME_SOURCE=%DEPLOYMENT_DIR%\ZXing.Net.Source.%VERSION%.zip
 SET ZIP_TOOL=%CD%\3rdparty\zip\7za.exe
 SET SVN_EXPORT_DIR=%DEPLOYMENT_DIR%\Source
-SET SVN_URL=https://zxingnet.svn.codeplex.com/svn/trunk/Source/lib
+SET SVN_URL=https://zxingnet.svn.codeplex.com/svn/trunk
 SET SVN_TOOL=%CD%\3rdparty\Subversion\svn.exe
 
 ECHO.
@@ -19,11 +19,20 @@ ECHO Build deployment files in directory
 ECHO %DEPLOYMENT_DIR%...
 ECHO.
 
+REM
+REM preparing deployment directory
+REM ***************************************************************************************
+
 IF NOT EXIST "%BUILD_DIR%" GOTO BUILD_DIR_NOT_FOUND
 IF NOT EXIST "%BINARY_DIR%" GOTO BINARY_DIR_NOT_FOUND
 
 MKDIR "%DEPLOYMENT_DIR%" >NUL: 2>&1
 DEL /F "%DEPLOYMENT_DIR%\%FILENAME_BINARY%" >NUL: 2>&1
+
+
+REM
+REM building archives for binaries
+REM ***************************************************************************************
 
 CD "%BINARY_DIR%"
 "%ZIP_TOOL%" a -tzip -mx9 -r "%FILENAME_BINARY%" net2.0 net4.0 unity sl4 sl5 wp7.0 wp7.1
@@ -31,20 +40,41 @@ CD "%BINARY_DIR%"
 CD "%CURRENT_DIR%"
 
 ECHO.
+
+
+REM
+REM building nuget archive
+REM ***************************************************************************************
+
 CALL nuget-pack.cmd
 
 ECHO.
 
-MKDIR "%SVN_EXPORT_DIR%" >NUL: 2>&1
-RMDIR /S /Q "%SVN_EXPORT_DIR%"
 
-"%SVN_TOOL%" export "%SVN_URL%" "%SVN_EXPORT_DIR%"
+REM
+REM building source archive
+REM ***************************************************************************************
+
+RMDIR /S /Q "%SVN_EXPORT_DIR%" >NUL: 2>&1
+
+MKDIR "%SVN_EXPORT_DIR%" >NUL: 2>&1
+MKDIR "%SVN_EXPORT_DIR%\Source" >NUL: 2>&1
+MKDIR "%SVN_EXPORT_DIR%\Source\lib" >NUL: 2>&1
+MKDIR "%SVN_EXPORT_DIR%\Source\test" >NUL: 2>&1
+MKDIR "%SVN_EXPORT_DIR%\Source\test\src" >NUL: 2>&1
+MKDIR "%SVN_EXPORT_DIR%\Clients" >NUL: 2>&1
+
+"%SVN_TOOL%" export --force "%SVN_URL%/Source/lib" "%SVN_EXPORT_DIR%\Source\lib"
+"%SVN_TOOL%" export --force "%SVN_URL%/Source/test/src" "%SVN_EXPORT_DIR%\Source\test\src"
+"%SVN_TOOL%" export --force "%SVN_URL%/Clients" "%SVN_EXPORT_DIR%\Clients"
+"%SVN_TOOL%" export --force "%SVN_URL%/zxing.sln" "%SVN_EXPORT_DIR%"
 
 CD "%SVN_EXPORT_DIR%"
-"%ZIP_TOOL%" a -tzip -mx9 -r "%FILENAME_SOURCE%" *.*
+"%ZIP_TOOL%" a -tzip -mx9 -r "%FILENAME_SOURCE%" Source\lib\*.* Source\test\src\*.* Clients\*.* zxing.sln
 CD "%CURRENT_DIR%"
 
-RMDIR /S /Q "%SVN_EXPORT_DIR%"
+RMDIR /S /Q "%SVN_EXPORT_DIR%" >NUL: 2>&1
+
 
 GOTO END
 
