f5c2b0246f727b2daaf8b700e7c78de55359a23a
micjahn <mikej06@hotmail.com>
2012-08-06 21:41:33 +0000

    added BitmapSource support to RGBLuminanceSource (.Net 4.0 target)
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C80874

---
+++ Source/lib/RGBLuminanceSource.Presentation.cs
@@ -0,0 +1,129 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ZXing
+{
+   public partial class RGBLuminanceSource
+   {
+      /// <summary>
+      /// Initializes a new instance of the <see cref="RGBLuminanceSource"/> class.
+      /// </summary>
+      /// <param name="bitmap">The bitmap.</param>
+      public RGBLuminanceSource(BitmapSource bitmap)
+         : base(bitmap.PixelWidth, bitmap.PixelHeight)
+      {
+         switch (bitmap.Format.ToString())
+         {
+            case "Bgr24":
+            case "Bgr32":
+            case "Bgra32":
+               CalculateLuminanceBGR(bitmap);
+               break;
+            case "Rgb24":
+               CalculateLuminanceRGB(bitmap);
+               break;
+            case "Bgr565":
+               CalculateLuminanceBGR565(bitmap);
+               break;
+            default:
+               // there is no special conversion routine to luminance values
+               // we have to convert the image to a supported format
+               bitmap = new FormatConvertedBitmap(bitmap, PixelFormats.Bgra32, null, 0);
+               CalculateLuminanceBGR(bitmap);
+               break;
+         }
+      }
+
+      private void CalculateLuminanceRGB(BitmapSource bitmap)
+      {
+         var width = bitmap.PixelWidth;
+         var height = bitmap.PixelHeight;
+         var stepX = (bitmap.Format.BitsPerPixel + 7) / 8;
+         var bufferSize = width * stepX;
+         var buffer = new byte[bufferSize];
+         var rect = new Int32Rect(0, 0, width, 1);
+         var luminanceIndex = 0;
+
+         luminances = new byte[width * height];
+
+         for (var curY = 0; curY < height; curY++)
+         {
+            bitmap.CopyPixels(rect, buffer, bufferSize, 0);
+            for (var curX = 0; curX < bufferSize; curX += stepX)
+            {
+               var r = buffer[curX];
+               var g = buffer[curX + 1];
+               var b = buffer[curX + 2];
+               luminances[luminanceIndex] = (byte)
+                  (0.3 * r + 0.59 * g + 0.11 * b + 0.01);
+               luminanceIndex++;
+            }
+            rect.Y++;
+         }
+      }
+
+      private void CalculateLuminanceBGR(BitmapSource bitmap)
+      {
+         var width = bitmap.PixelWidth;
+         var height = bitmap.PixelHeight;
+         var stepX = (bitmap.Format.BitsPerPixel + 7) / 8;
+         var bufferSize = width * stepX;
+         var buffer = new byte[bufferSize];
+         var rect = new Int32Rect(0, 0, width, 1);
+         var luminanceIndex = 0;
+
+         luminances = new byte[width * height];
+
+         for (var curY = 0; curY < height; curY++)
+         {
+            bitmap.CopyPixels(rect, buffer, bufferSize, 0);
+            for (var curX = 0; curX < bufferSize; curX += stepX)
+            {
+               var b = buffer[curX];
+               var g = buffer[curX + 1];
+               var r = buffer[curX + 2];
+               luminances[luminanceIndex] = (byte)
+                  (0.3 * r + 0.59 * g + 0.11 * b + 0.01);
+               luminanceIndex++;
+            }
+            rect.Y++;
+         }
+      }
+
+      private void CalculateLuminanceBGR565(BitmapSource bitmap)
+      {
+         var width = bitmap.PixelWidth;
+         var height = bitmap.PixelHeight;
+         var stepX = (bitmap.Format.BitsPerPixel + 7) / 8;
+         var bufferSize = width * stepX;
+         var buffer = new byte[bufferSize];
+         var rect = new Int32Rect(0, 0, width, 1);
+         var luminanceIndex = 0;
+
+         luminances = new byte[width * height];
+
+         for (var curY = 0; curY < height; curY++)
+         {
+            bitmap.CopyPixels(rect, buffer, bufferSize, 0);
+            for (var curX = 0; curX < bufferSize; curX += stepX)
+            {
+               var byte1 = buffer[curX];
+               var byte2 = buffer[curX + 1];
+               // cheap, not fully accurate conversion
+               var b5 = byte1 & 0x1F;
+               var g5 = (((byte1 & 0xE0) >> 5) | ((byte2 & 0x03) << 3)) & 0x1F;
+               var r5 = (byte2 >> 2) & 0x1F;
+               var r8 = (r5 * 527 + 23) >> 6;
+               var g8 = (g5 * 527 + 23) >> 6;
+               var b8 = (b5 * 527 + 23) >> 6;
+
+               luminances[luminanceIndex] = (byte)
+                  (0.3 * r8 + 0.59 * g8 + 0.11 * b8 + 0.01);
+               luminanceIndex++;
+            }
+            rect.Y++;
+         }
+      }
+   }
+}
---
+++ Source/test/src/RGBLuminanceSourceTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using NUnit.Framework;
+
+namespace ZXing.Test
+{
+   [TestFixture]
+   public class RGBLuminanceSourceTests
+   {
+      private const string samplePicRelPath = @"../../../Source/test/data/blackbox/codabar-1/01.jpg";
+      private LuminanceSource samplePicLuminance;
+
+      [SetUp]
+      public void Setup()
+      {
+         var bitmap = (Bitmap)Image.FromFile(samplePicRelPath);
+         samplePicLuminance = new RGBLuminanceSource(bitmap);
+      }
+
+      [Test]
+      public void RGBLuminanceSource_Should_Work_With_BitmapImage()
+      {
+         var pixelFormats = new []
+                               {
+                                  PixelFormats.Bgr24,
+                                  PixelFormats.Bgr32,
+                                  PixelFormats.Bgra32,
+                                  PixelFormats.Rgb24,
+                                  //PixelFormats.Bgr565, // conversion isn't accurate to compare it directly to RGB24
+                                  //PixelFormats.Bgr555, // conversion isn't accurate to compare it directly to RGB24
+                                  PixelFormats.Gray8,
+                               };
+         foreach (var pixelFormat in pixelFormats)
+         {
+            BitmapSource bitmapImage = new BitmapImage(new Uri(samplePicRelPath, UriKind.RelativeOrAbsolute));
+            if (bitmapImage.Format != pixelFormat)
+               bitmapImage = new FormatConvertedBitmap(bitmapImage, pixelFormat, null, 0);
+            var rgbLuminanceSource = new RGBLuminanceSource(bitmapImage);
+            Assert.That(IsEqual(samplePicLuminance.Matrix, rgbLuminanceSource.Matrix));
+         }
+      }
+
+      private static bool IsEqual(byte[] left, byte[] right)
+      {
+         if (left.Length != right.Length)
+            return false;
+
+         for (var index = 0; index < left.Length; index++)
+         {
+            if (left[index] != right[index])
+               return false;
+         }
+
+         return true;
+      }
+   }
+}
