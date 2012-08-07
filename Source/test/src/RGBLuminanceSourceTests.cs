e61b4aa2d91f8cc04ef293e8fd48e49c4e02ecd1
micjahn <mikej06@hotmail.com>
2012-08-07 18:55:05 +0000

    changed RGBLuminanceSourceTests to use own test data
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C80886

--- Source/test/src/RGBLuminanceSourceTests.cs
+++ Source/test/src/RGBLuminanceSourceTests.cs
@@ -1,8 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿/*
+* Copyright 2012 ZXing.Net authors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.IO;
+using System.IO.Compression;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using NUnit.Framework;
@@ -12,14 +26,17 @@ namespace ZXing.Test
    [TestFixture]
    public class RGBLuminanceSourceTests
    {
-      private const string samplePicRelPath = @"../../../Source/test/data/blackbox/codabar-1/01.jpg";
-      private LuminanceSource samplePicLuminance;
+      private const string samplePicRelPath = @"../../../Source/test/data/luminance/01.jpg";
+      private const string samplePicRelResultPath = @"../../../Source/test/data/luminance/01Result.txt.gz";
+      private string samplePicRelResult;
 
       [SetUp]
       public void Setup()
       {
-         var bitmap = (Bitmap)Image.FromFile(samplePicRelPath);
-         samplePicLuminance = new RGBLuminanceSource(bitmap);
+         using (var stream = File.OpenRead(samplePicRelResultPath))
+         using (var deflateStream = new GZipStream(stream, CompressionMode.Decompress, true))
+         using (var reader = new StreamReader(deflateStream))
+            samplePicRelResult = reader.ReadToEnd();
       }
 
       [Test]
@@ -41,22 +58,9 @@ namespace ZXing.Test
             if (bitmapImage.Format != pixelFormat)
                bitmapImage = new FormatConvertedBitmap(bitmapImage, pixelFormat, null, 0);
             var rgbLuminanceSource = new RGBLuminanceSource(bitmapImage);
-            Assert.That(IsEqual(samplePicLuminance.Matrix, rgbLuminanceSource.Matrix));
+            var rgbLuminanceSourceResult = rgbLuminanceSource.ToString();
+            Assert.That(samplePicRelResult.Equals(rgbLuminanceSourceResult));
          }
-      }
-
-      private static bool IsEqual(byte[] left, byte[] right)
-      {
-         if (left.Length != right.Length)
-            return false;
-
-         for (var index = 0; index < left.Length; index++)
-         {
-            if (left[index] != right[index])
-               return false;
-         }
-
-         return true;
       }
    }
 }
