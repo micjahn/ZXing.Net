dcf09ae630dd83c520bc331fa30514e356fd0799
micjahn <mikej06@hotmail.com>
2012-09-19 21:05:38 +0000

    * fixed Render call in BarcodeWriterGeneric which doesn't set the options argument * extended BarcodeWriterGeneric with a Encoder property and added comments
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C81577

--- Source/lib/BarcodeWriterGeneric.cs
+++ Source/lib/BarcodeWriterGeneric.cs
@@ -15,27 +15,29 @@
  */
 
 using System;
-#if !(SILVERLIGHT || NETFX_CORE)
-#if !UNITY
-using System.Drawing;
-#endif
-#elif NETFX_CORE
-using Windows.UI.Xaml.Media.Imaging;
-#else
-using System.Windows.Media.Imaging;
-#endif
 
 using ZXing.Common;
 using ZXing.Rendering;
 
 namespace ZXing
 {
+   /// <summary>
+   /// A base class for specific barcode writers with specific formats of barcode images.
+   /// </summary>
+   /// <typeparam name="TOutput">The type of the output.</typeparam>
    public class BarcodeWriterGeneric<TOutput> : IBarcodeWriterGeneric<TOutput>
    {
       private EncodingOptions options;
 
+      /// <summary>
+      /// Gets or sets the barcode format.
+      /// The value is only suitable if the MultiFormatWriter is used.
+      /// </summary>
       public BarcodeFormat Format { get; set; }
 
+      /// <summary>
+      /// Gets or sets the options container for the encoding and renderer process.
+      /// </summary>
       public EncodingOptions Options
       {
          get
@@ -48,15 +50,37 @@ namespace ZXing
          }
       }
 
+      /// <summary>
+      /// Gets or sets the writer which encodes the content to a BitMatrix.
+      /// If no value is set the MultiFormatWriter is used.
+      /// </summary>
+      public Writer Encoder { get; set; }
+
+      /// <summary>
+      /// Gets or sets the renderer which should be used to render the encoded BitMatrix.
+      /// </summary>
       public IBarcodeRenderer<TOutput> Renderer { get; set; }
 
+      /// <summary>
+      /// Encodes the specified contents and returns a BitMatrix array.
+      /// That array has to be rendered manually or with a IBarcodeRenderer.
+      /// </summary>
+      /// <param name="contents">The contents.</param>
+      /// <returns></returns>
       public BitMatrix Encode(string contents)
       {
-         var encoder = new MultiFormatWriter();
-         var options = Options ?? new EncodingOptions {Height = 100, Width = 100};
-         return encoder.encode(contents, Format, options.Width, options.Height, options.Hints);
+         var encoder = Encoder ?? new MultiFormatWriter();
+         var currentOptions = Options;
+         return encoder.encode(contents, Format, currentOptions.Width, currentOptions.Height, currentOptions.Hints);
       }
 
+      /// <summary>
+      /// Encodes the specified contents and returns a rendered instance of the barcode.
+      /// For rendering the instance of the property Renderer is used and has to be set before
+      /// calling that method.
+      /// </summary>
+      /// <param name="contents">The contents.</param>
+      /// <returns></returns>
       public TOutput Write(string contents)
       {
          if (Renderer == null)
@@ -66,7 +90,7 @@ namespace ZXing
 
          var matrix = Encode(contents);
 
-         return Renderer.Render(matrix, Format, contents);
+         return Renderer.Render(matrix, Format, contents, Options);
       }
    }
 }
