3b630ba99af4d5608f93c93cdcda76138366b42f
micjahn <mikej06@hotmail.com>
2012-12-20 19:16:49 +0000

    added Windows RT demo (thanks to jfren484)
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C84094

---
+++ Clients/WindowsRTDemo/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WindowsRT
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                Window.Current.Content = rootFrame = new Frame();
+            }
+
+            if (rootFrame.Content == null)
+            {
+                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+
+            Window.Current.Activate();
+        }
+    }
+}
---
+++ Clients/WindowsRTDemo/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using ZXing;
+
+namespace WindowsRT
+{
+   public sealed partial class MainPage : Page
+   {
+      private readonly MediaCapture _mediaCapture = new MediaCapture();
+      private Result _result;
+
+      public MainPage()
+      {
+         InitializeComponent();
+      }
+
+      protected override async void OnNavigatedTo(NavigationEventArgs e)
+      {
+         try
+         {
+            var cameras = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+            if (cameras.Count < 1)
+            {
+               ScanResult.Text = "No camera found";
+               return;
+            }
+            var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameras[1].Id }; // 0 => front, 1 => back
+
+            await _mediaCapture.InitializeAsync(settings);
+            VideoCapture.Source = _mediaCapture;
+            await _mediaCapture.StartPreviewAsync();
+
+            while (_result == null)
+            {
+               var photoStorageFile = await KnownFolders.PicturesLibrary.CreateFileAsync("scan.jpg", CreationCollisionOption.GenerateUniqueName);
+               await _mediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreateJpeg(), photoStorageFile);
+
+               var writeableBmp = new WriteableBitmap(640, 360);
+               writeableBmp.SetSource(await photoStorageFile.OpenReadAsync());
+
+               var barcodeReader = new BarcodeReader
+               {
+                  TryHarder = true,
+                  AutoRotate = true
+               };
+               _result = barcodeReader.Decode(writeableBmp);
+
+               if (_result != null)
+               {
+                  CaptureImage.Source = writeableBmp;
+               }
+
+               await photoStorageFile.DeleteAsync(StorageDeleteOption.PermanentDelete);
+            }
+
+            await _mediaCapture.StopPreviewAsync();
+            VideoCapture.Visibility = Visibility.Collapsed;
+            CaptureImage.Visibility = Visibility.Visible;
+            ScanResult.Text = _result.Text;
+         }
+         catch (Exception ex)
+         {
+            ScanResult.Text = ex.Message;
+         }
+      }
+
+      protected override async void OnNavigatingFrom(NavigatingCancelEventArgs e)
+      {
+         await _mediaCapture.StopPreviewAsync();
+      }
+   }
+}
