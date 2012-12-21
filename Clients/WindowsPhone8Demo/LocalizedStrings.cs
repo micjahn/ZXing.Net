31267883b389a0cbbf8afc17d14efc4c64a1d2a5
micjahn <mikej06@hotmail.com>
2012-12-21 13:16:54 +0000

    added Windows Phone 8 Demo (mainly a copy of the Windows Phone 7 demo)
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C84110

---
+++ Clients/WindowsPhone8Demo/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WindowsPhone8Demo.Resources;
+
+namespace WindowsPhone8Demo
+{
+   /// <summary>
+   /// Bietet Zugriff auf Zeichenfolgenressourcen.
+   /// </summary>
+   public class LocalizedStrings
+   {
+      private static AppResources _localizedResources = new AppResources();
+
+      public AppResources LocalizedResources { get { return _localizedResources; } }
+   }
+}
