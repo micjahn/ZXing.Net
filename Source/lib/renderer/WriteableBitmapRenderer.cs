168bd19a732cd2a75883e0dec26925c38f621153
micjahn <mikej06@hotmail.com>
2013-01-29 21:31:01 +0000

    fixed NullRef in renderer classes
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C84770

--- Source/lib/renderer/WriteableBitmapRenderer.cs
+++ Source/lib/renderer/WriteableBitmapRenderer.cs
@@ -130,7 +130,8 @@ namespace ZXing.Rendering
       {
          int width = matrix.Width;
          int height = matrix.Height;
-         bool outputContent = !options.PureBarcode && !String.IsNullOrEmpty(content) && (format == BarcodeFormat.CODE_39 ||
+         bool outputContent = (options == null || !options.PureBarcode) &&
+                              !String.IsNullOrEmpty(content) && (format == BarcodeFormat.CODE_39 ||
                                                                  format == BarcodeFormat.CODE_128 ||
                                                                  format == BarcodeFormat.EAN_13 ||
                                                                  format == BarcodeFormat.EAN_8 ||
