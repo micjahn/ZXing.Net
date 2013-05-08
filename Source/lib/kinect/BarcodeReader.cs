5956efd34b45fff79562cef80f6d94e53cf05a42
micjahn <mikej06@hotmail.com>
2013-05-08 19:41:57 +0000

    added kinect support
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C86128

--- Source/lib/kinect/BarcodeReader.cs
+++ Source/lib/kinect/BarcodeReader.cs
@@ -1,31 +1,32 @@
 ﻿/*
-* Copyright 2012 ZXing.Net authors
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2012 ZXing.Net authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 using System;
-using System.Windows.Media.Imaging;
 
-namespace ZXing.Presentation
+using Microsoft.Kinect;
+
+namespace ZXing.Kinect
 {
    /// <summary>
-   /// A smart class to decode the barcode inside a bitmap object which is derived from BitmapSource
+   /// A smart class to decode the barcode inside a ColorImageFrame object
    /// </summary>
-   public class BarcodeReader : BarcodeReaderGeneric<BitmapSource>
+   public class BarcodeReader : BarcodeReaderGeneric<ColorImageFrame>
    {
-      private static readonly Func<BitmapSource, LuminanceSource> defaultCreateLuminanceSource =
-         bitmap => new BitmapSourceLuminanceSource(bitmap);
+      private static readonly Func<ColorImageFrame, LuminanceSource> defaultCreateLuminanceSource =
+         bitmap => new ColorImageFrameLuminanceSource(bitmap);
 
       /// <summary>
       /// Initializes a new instance of the <see cref="BarcodeReader"/> class.
@@ -40,12 +41,12 @@ namespace ZXing.Presentation
       /// </summary>
       /// <param name="reader">Sets the reader which should be used to find and decode the barcode.
       /// If null then MultiFormatReader is used</param>
-      /// <param name="createLuminanceSource">Sets the function to create a luminance source object for a bitmap.
+      /// <param name="createLuminanceSource">Sets the function to create a luminance source object for a ColorImageFrame.
       /// If null, default is used</param>
       /// <param name="createBinarizer">Sets the function to create a binarizer object for a luminance source.
       /// If null then HybridBinarizer is used</param>
       public BarcodeReader(Reader reader,
-         Func<BitmapSource, LuminanceSource> createLuminanceSource,
+         Func<ColorImageFrame, LuminanceSource> createLuminanceSource,
          Func<LuminanceSource, Binarizer> createBinarizer
          )
          : base(reader, createLuminanceSource ?? defaultCreateLuminanceSource, createBinarizer)
@@ -61,9 +62,8 @@ namespace ZXing.Presentation
       /// If null, default is used</param>
       /// <param name="createBinarizer">Sets the function to create a binarizer object for a luminance source.
       /// If null then HybridBinarizer is used</param>
-      /// <param name="createRGBLuminanceSource">The create RGB luminance source.</param>
       public BarcodeReader(Reader reader,
-         Func<BitmapSource, LuminanceSource> createLuminanceSource,
+         Func<ColorImageFrame, LuminanceSource> createLuminanceSource,
          Func<LuminanceSource, Binarizer> createBinarizer,
          Func<byte[], int, int, RGBLuminanceSource.BitmapFormat, LuminanceSource> createRGBLuminanceSource
          )
---
+++ Source/lib/kinect/ColorImageFrameLuminanceSource.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Copyright 2012 ZXing.Net authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Microsoft.Kinect;
+
+namespace ZXing.Kinect
+{
+   /// <summary>
+   /// special luminance class which supports ColorImageFrame directly
+   /// </summary>
+   public class ColorImageFrameLuminanceSource : RGBLuminanceSource
+   {
+      /// <summary>
+      /// Initializes a new instance of the <see cref="ColorImageFrameLuminanceSource"/> class.
+      /// </summary>
+      /// <param name="width">The width.</param>
+      /// <param name="height">The height.</param>
+      protected ColorImageFrameLuminanceSource(int width, int height)
+         : base(width, height)
+      {
+      }
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="ColorImageFrameLuminanceSource"/> class.
+      /// </summary>
+      /// <param name="bitmap">The bitmap.</param>
+      public ColorImageFrameLuminanceSource(ColorImageFrame bitmap)
+         : base(bitmap.Width, bitmap.Height)
+      {
+         var pixelData = new byte[bitmap.PixelDataLength];
+         var bitmapFormat = BitmapFormat.Unknown;
+         switch (bitmap.Format)
+         {
+            case ColorImageFormat.InfraredResolution640x480Fps30:
+               // not sure, what BitmapFormat should be selected
+               break;
+            case ColorImageFormat.RawBayerResolution1280x960Fps12:
+            case ColorImageFormat.RawBayerResolution640x480Fps30:
+               // not sure, what BitmapFormat should be selected
+               break;
+            case ColorImageFormat.RgbResolution1280x960Fps12:
+            case ColorImageFormat.RgbResolution640x480Fps30:
+               bitmapFormat = BitmapFormat.RGB32;
+               break;
+            case ColorImageFormat.RawYuvResolution640x480Fps15:
+            case ColorImageFormat.YuvResolution640x480Fps15:
+               // not sure, what BitmapFormat should be selected
+               break;
+            default:
+               break;
+         }
+         bitmap.CopyPixelDataTo(pixelData);
+         CalculateLuminance(pixelData, bitmapFormat);
+      }
+
+      protected override LuminanceSource CreateLuminanceSource(byte[] newLuminances, int width, int height)
+      {
+         return new ColorImageFrameLuminanceSource(width, height) { luminances = newLuminances };
+      }
+   }
+}
