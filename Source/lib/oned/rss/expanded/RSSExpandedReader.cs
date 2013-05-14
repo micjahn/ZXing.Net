24c53dab3ebc5cd6e4ebd881ce5f86f32fbec8b0
micjahn <mikej06@hotmail.com>
2013-05-14 19:06:47 +0000

    port rev. 2705: Minor inspection cleanup
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C86199

--- Source/lib/oned/rss/expanded/RSSExpandedReader.cs
+++ Source/lib/oned/rss/expanded/RSSExpandedReader.cs
@@ -756,7 +756,7 @@ namespace ZXing.OneD.RSS.Expanded
             oddSum += oddCounts[i];
          }
          int evenChecksumPortion = 0;
-         int evenSum = 0;
+         //int evenSum = 0;
          for (int i = evenCounts.Length - 1; i >= 0; i--)
          {
             if (isNotA1left(pattern, isOddPattern, leftChar))
@@ -764,7 +764,7 @@ namespace ZXing.OneD.RSS.Expanded
                int weight = WEIGHTS[weightRowNumber][2 * i + 1];
                evenChecksumPortion += evenCounts[i] * weight;
             }
-            evenSum += evenCounts[i];
+            //evenSum += evenCounts[i];
          }
          int checksumPortion = oddChecksumPortion + evenChecksumPortion;
 
