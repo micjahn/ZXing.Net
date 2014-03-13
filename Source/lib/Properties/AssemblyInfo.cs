4fa1f8569529bbc5185bab0794369a87a1fc65a8
micjahn <mikej06@hotmail.com>
2014-03-13 21:01:16 +0000

    port SHA-1: f0532a273031f71e75110276833164156c768a6f
    * Issue #86 : Fix logic error in Code 39 that was requiring too much quiet zone
    
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C88419

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -40,7 +40,7 @@ using System.Runtime.InteropServices;
 #if MONOTOUCH
 [assembly: AssemblyTitle("zxing.net for mono touch")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 25.02.2014 10:38:23)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 13.03.2014 15:19:08)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012-2014")]
