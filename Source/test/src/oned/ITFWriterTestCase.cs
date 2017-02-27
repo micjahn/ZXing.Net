55c41b670881ecb3c5f7dc980dfc6f6568bfb840
micjahn_cp <091957ed9bc2beaf9976b8124634cc4bfb41899c8UyuZhmm>
2017-02-27 20:02:45 +0000

    port SHA-1: 6546dc5863ac50a2b1a43925d8ab4fe313a887dd
    * Add basic test case for ITFWriter (#758)
    
    
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C90107

---
+++ Source/test/src/oned/ITFWriterTestCase.cs
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+
+using NUnit.Framework;
+
+using ZXing.Common.Test;
+
+namespace ZXing.OneD.Test
+{
+   public class ITFWriterTestCase
+   {
+      [TestCase("00123456789012", "0000010101010111000111000101110100010101110001110111010001010001110100011100010101000101011100011101011101000111000101110100010101110001110100000", TestName = "ITFtestEncode")]
+      public void testEncode(String input, String expected)
+      {
+         var result = new ITFWriter().encode(input, BarcodeFormat.ITF, 0, 0);
+         Assert.AreEqual(expected, BitMatrixTestCase.matrixToString(result));
+      }
+   }
+}
