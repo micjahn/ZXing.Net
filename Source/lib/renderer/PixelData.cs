6104818a17381f03b9027cfc1024b3df51146ec7
micjahn_cp <091957ed9bc2beaf9976b8124634cc4bfb41899c8UyuZhmm>
2017-04-24 20:17:58 +0000

    splitting up parts of the #if-hell into a source-file-hell (part 1)
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C90182

--- Source/lib/renderer/PixelData.cs
+++ Source/lib/renderer/PixelData.cs
@@ -14,11 +14,12 @@
  * limitations under the License.
  */
 
-using System.IO;
-
 namespace ZXing.Rendering
 {
-   public sealed class PixelData
+   /// <summary>
+   /// represents the generated code as a byte array with pixel data (4 byte per pixel, BGRA)
+   /// </summary>
+   public sealed partial class PixelData
    {
       internal PixelData(int width, int height, byte[] pixels)
       {
@@ -27,93 +28,17 @@ namespace ZXing.Rendering
          Pixels = pixels;
       }
 
+      /// <summary>
+      /// the generated code as byte array of BGRA pixels
+      /// </summary>
       public byte[] Pixels { get; private set; }
+      /// <summary>
+      /// the width of the image
+      /// </summary>
       public int Width { get; private set; }
+      /// <summary>
+      /// the height of the image
+      /// </summary>
       public int Height { get; private set; }
-
-#if (NET45 || NET40 || NET35 || NET20 || WindowsCE) && !UNITY
-      public System.Drawing.Bitmap ToBitmap()
-      {
-#if WindowsCE
-         var bmp = new System.Drawing.Bitmap(Width, Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
-         var bmpData = bmp.LockBits(
-            new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height),
-            System.Drawing.Imaging.ImageLockMode.WriteOnly,
-            System.Drawing.Imaging.PixelFormat.Format32bppRgb);
-#else
-         var bmp = new System.Drawing.Bitmap(Width, Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-         bmp.SetResolution(96, 96);
-         var bmpData = bmp.LockBits(
-            new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), 
-            System.Drawing.Imaging.ImageLockMode.WriteOnly, 
-            System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-#endif
-         try
-         {
-            //Copy the data from the byte array into BitmapData.Scan0
-            System.Runtime.InteropServices.Marshal.Copy(Pixels, 0, bmpData.Scan0, Pixels.Length);
-         }
-         finally
-         {
-            //Unlock the pixels
-            bmp.UnlockBits(bmpData);
-         }
-
-         return bmp;
-      }
-#endif
-
-#if UNITY
-      // Unity3D
-#endif
-
-#if NETFX_CORE
-      public Windows.UI.Xaml.Media.Imaging.WriteableBitmap ToBitmap()
-      {
-         var bmp = new Windows.UI.Xaml.Media.Imaging.WriteableBitmap(Width, Height);
-         using (var stream = System.Runtime.InteropServices.WindowsRuntime.WindowsRuntimeBufferExtensions.AsStream(bmp.PixelBuffer))
-         {
-            stream.Write(Pixels, 0, Pixels.Length);
-         }
-         bmp.Invalidate();
-         return bmp;
-      }
-#endif
-
-#if SILVERLIGHT
-      public System.Windows.Media.Imaging.WriteableBitmap ToBitmap()
-      {
-         var bmp = new System.Windows.Media.Imaging.WriteableBitmap(Width, Height);
-         bmp.SetSource(new MemoryStream(Pixels));
-         bmp.Invalidate();
-         return bmp;
-      }
-#endif
-
-#if MONOANDROID
-      public Android.Graphics.Bitmap ToBitmap()
-      {
-         var pixels = Pixels;
-         var colors = new int[Width*Height];
-         for (var index = 0; index < Width*Height; index++)
-         {
-            colors[index] =
-               pixels[index*4] << 24 |
-               pixels[index*4 + 1] << 16 |
-               pixels[index*4 + 2] << 8 |
-               pixels[index*4 + 3];
-         }
-         return Android.Graphics.Bitmap.CreateBitmap(colors, Width, Height, Android.Graphics.Bitmap.Config.Argb8888);
-      }
-#endif
-
-#if MONOTOUCH
-#if __UNIFIED__
-using UIKit;
-#else
-using MonoTouch.UIKit;
-#endif
-#endif
-
    }
 }
