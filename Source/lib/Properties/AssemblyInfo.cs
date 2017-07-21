68af444536189032ee1d6a070ac5d24955029cb7
micjahn_cp <091957ed9bc2beaf9976b8124634cc4bfb41899c8UyuZhmm>
2017-07-21 07:09:27 +0000

    port SHA-1: ae6b9f4426996d09fd3942670dae8d88e0ca9bd5
    * A few more touch-ups from Codacy
    
    
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C90258

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -61,7 +61,7 @@ using System.Runtime.InteropServices;
 #if NETFX_CORE && !WINDOWS_UWP
 [assembly: AssemblyTitle("zxing.net for windows rt")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 14.06.2017 19:49:57)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 16.06.2017 14:51:02)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
