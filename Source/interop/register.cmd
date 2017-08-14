f18c14c3e7992e8ea6c44fd403286cec52037f25
micjahn_cp <091957ed9bc2beaf9976b8124634cc4bfb41899c8UyuZhmm>
2017-08-14 20:15:59 +0000

    updates register.cmd for com interop
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C90277

--- Source/interop/register.cmd
+++ Source/interop/register.cmd
@@ -1,5 +1,7 @@
 @ECHO OFF
 
-"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe" /codebase %~dp0zxing.interop.dll
+"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe" /unregister %~dp0zxing.interop.dll
+
+"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe" /codebase %~dp0zxing.interop.dll /tlb %~dp0zxing.interop.tlb
 
 pause
