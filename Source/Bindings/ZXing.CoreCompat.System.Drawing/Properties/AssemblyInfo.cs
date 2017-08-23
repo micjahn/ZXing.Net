cb2460ef0667d845eef3d2da3424bddd8fb2ac52
micjahn_cp <091957ed9bc2beaf9976b8124634cc4bfb41899c8UyuZhmm>
2017-08-23 20:23:52 +0000

    #35 CoreCompat adapter for NET Core 2.0
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C90285

--- Source/Bindings/ZXing.CoreCompat.System.Drawing/Properties/AssemblyInfo.cs
+++ Source/Bindings/ZXing.CoreCompat.System.Drawing/Properties/AssemblyInfo.cs
@@ -5,9 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#if !NETSTANDARD2_0
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("zxing.corecompat.system.drawing")]
+#endif
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
--- Source/Bindings/ZXing.CoreCompat.System.Drawing/nuget-pack.cmd
+++ Source/Bindings/ZXing.CoreCompat.System.Drawing/nuget-pack.cmd
@@ -1,6 +1,10 @@
 @ECHO OFF
 
+SETLOCAL
+
 SET OUTDIR=..\..\..\Build\Deployment
 mkdir %OUTDIR%
 
 ..\..\..\3rdParty\nuget\nuget pack project.nuspec -outputdirectory %OUTDIR%
+
+ENDLOCAL
