0aebf4962c393f96bddc72e6cea87c1b743e7b0c
micjahn_cp <091957ed9bc2beaf9976b8124634cc4bfb41899c8UyuZhmm>
2017-09-11 20:53:22 +0000

    fix deployment files
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C90307

--- nuget-push.cmd
+++ nuget-push.cmd
@@ -1,17 +1,9 @@
 @ECHO OFF
 
-SET Id=ZXing.Net
-SET VERSION=0.16.1
-
 echo Next step - uploading all nuget packages to nuget.org...
 pause
 
-3rdParty\nuget\nuget push Build\Deployment\%ID%.%VERSION%.nupkg -Source https://www.nuget.org/api/v2/package
-3rdParty\nuget\nuget push Build\Deployment\%ID%.Bindings.CoreCompat.System.Drawing.%VERSION%-beta.nupkg -Source https://www.nuget.org/api/v2/package
-3rdParty\nuget\nuget push Build\Deployment\%ID%.Bindings.ImageSharp.%VERSION%-beta.nupkg -Source https://www.nuget.org/api/v2/package
-3rdParty\nuget\nuget push Build\Deployment\%ID%.Bindings.Kinect.V1.%VERSION%.nupkg -Source https://www.nuget.org/api/v2/package
-3rdParty\nuget\nuget push Build\Deployment\%ID%.Bindings.Kinect.V2.%VERSION%.nupkg -Source https://www.nuget.org/api/v2/package
-3rdParty\nuget\nuget push Build\Deployment\%ID%.Bindings.Magick.%VERSION%.nupkg -Source https://www.nuget.org/api/v2/package
-3rdParty\nuget\nuget push Build\Deployment\%ID%.Bindings.OpenCVSharp.V2.%VERSION%.nupkg -Source https://www.nuget.org/api/v2/package
-3rdParty\nuget\nuget push Build\Deployment\%ID%.Bindings.OpenCV.%VERSION%.nupkg -Source https://www.nuget.org/api/v2/package
-3rdParty\nuget\nuget push Build\Deployment\%ID%.Bindings.SkiaSharp.%VERSION%.nupkg -Source https://www.nuget.org/api/v2/package
+FOR /f "tokens=*" %%f IN ('dir /b Build\Deployment\*.nupkg') DO (
+   echo %%f
+   3rdParty\nuget\nuget push Build\Deployment\%%f -Source https://www.nuget.org/api/v2/package
+)
