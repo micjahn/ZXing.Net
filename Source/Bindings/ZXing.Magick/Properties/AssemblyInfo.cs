5351eecb716857c3dd1022333c9abff8af3238f4
micjahn_cp <091957ed9bc2beaf9976b8124634cc4bfb41899c8UyuZhmm>
2017-10-05 19:40:21 +0000

    update Magick bindings
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/trunk;C90322

--- Source/Bindings/ZXing.Magick/Properties/AssemblyInfo.cs
+++ Source/Bindings/ZXing.Magick/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyDescription("ZXing.Net Bindings to Magick")]
-[assembly: AssemblyInformationalVersion("0.16.2")]
+[assembly: AssemblyInformationalVersion("0.16.4")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
