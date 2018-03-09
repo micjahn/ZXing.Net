2655181fddcd4264cd36ea6b5a8986df26446bbb
micjahn <mikej06@hotmail.com>
2018-03-09 20:56:05 +0100

    port SHA-1: a3bbebccba25273c5229d0904478a31eb9eecac9
    * ZXing 3.3.2 snapshot still creates wrong barcode for text with leadin… (#967)
    
    * ZXing 3.3.2 snapshot still creates wrong barcode for text with leading asterisk #960
    
    * missing spaces

--- Source/lib/datamatrix/encoder/EdifactEncoder.cs
+++ Source/lib/datamatrix/encoder/EdifactEncoder.cs
@@ -75,6 +75,12 @@ namespace ZXing.Datamatrix.Encoder
                context.updateSymbolInfo();
                int available = context.SymbolInfo.dataCapacity - context.CodewordCount;
                int remaining = context.RemainingCharacters;
+               // The following two lines are a hack inspired by the 'fix' from https://sourceforge.net/p/barcode4j/svn/221/
+               if (remaining > available)
+               {
+                  context.updateSymbolInfo(context.CodewordCount + 1);
+                  available = context.SymbolInfo.dataCapacity - context.CodewordCount;
+               }
                if (remaining <= available && available <= 2)
                {
                   return; //No unlatch
