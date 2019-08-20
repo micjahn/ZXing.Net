d2849a86bb8c6db69635b41bafce59892fc29585
Michael Jahn <mikej06@hotmail.com>
2019-08-20 21:33:09 +0200

    port SHA-1: 4d94ac22eb8cc7b0b5ec8176aab6b40db27e8db5
    * Add addition wifi: QR code test case from mailing list

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -61,7 +61,7 @@ using System.Runtime.InteropServices;
 #if NETFX_CORE && !WINDOWS_UWP
 [assembly: AssemblyTitle("zxing.net for windows rt")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 16.06.2019 17:10:23)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 29.07.2019 21:29:24)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
--- Source/test/src/client/result/WifiParsedResultTestCase.cs
+++ Source/test/src/client/result/WifiParsedResultTestCase.cs
@@ -20,78 +20,79 @@ using NUnit.Framework;
 
 namespace ZXing.Client.Result.Test
 {
-   /// <summary>
-   /// Tests <see cref="WifiParsedResult" />.
-   ///
-   /// <author>Vikram Aggarwal</author>
-   /// </summary>
-   [TestFixture]
-   public sealed class WifiParsedResultTestCase
-   {
+    /// <summary>
+    /// Tests <see cref="WifiParsedResult" />.
+    ///
+    /// <author>Vikram Aggarwal</author>
+    /// </summary>
+    [TestFixture]
+    public sealed class WifiParsedResultTestCase
+    {
 
-      [Test]
-      public void testNoPassword()
-      {
-         doTest("WIFI:S:NoPassword;P:;T:;;", "NoPassword", null, "nopass");
-         doTest("WIFI:S:No Password;P:;T:;;", "No Password", null, "nopass");
-      }
+        [Test]
+        public void testNoPassword()
+        {
+            doTest("WIFI:S:NoPassword;P:;T:;;", "NoPassword", null, "nopass");
+            doTest("WIFI:S:No Password;P:;T:;;", "No Password", null, "nopass");
+        }
 
-      [Test]
-      public void testWep()
-      {
-         doTest("WIFI:S:TenChars;P:0123456789;T:WEP;;", "TenChars", "0123456789", "WEP");
-         doTest("WIFI:S:TenChars;P:abcde56789;T:WEP;;", "TenChars", "abcde56789", "WEP");
-         // Non hex should not fail at this level
-         doTest("WIFI:S:TenChars;P:hellothere;T:WEP;;", "TenChars", "hellothere", "WEP");
+        [Test]
+        public void testWep()
+        {
+            doTest("WIFI:S:TenChars;P:0123456789;T:WEP;;", "TenChars", "0123456789", "WEP");
+            doTest("WIFI:S:TenChars;P:abcde56789;T:WEP;;", "TenChars", "abcde56789", "WEP");
+            // Non hex should not fail at this level
+            doTest("WIFI:S:TenChars;P:hellothere;T:WEP;;", "TenChars", "hellothere", "WEP");
 
-         // Escaped semicolons
-         doTest("WIFI:S:Ten\\;\\;Chars;P:0123456789;T:WEP;;", "Ten;;Chars", "0123456789", "WEP");
-         // Escaped colons
-         doTest("WIFI:S:Ten\\:\\:Chars;P:0123456789;T:WEP;;", "Ten::Chars", "0123456789", "WEP");
+            // Escaped semicolons
+            doTest("WIFI:S:Ten\\;\\;Chars;P:0123456789;T:WEP;;", "Ten;;Chars", "0123456789", "WEP");
+            // Escaped colons
+            doTest("WIFI:S:Ten\\:\\:Chars;P:0123456789;T:WEP;;", "Ten::Chars", "0123456789", "WEP");
 
-         // TODO(vikrama) Need a test for SB as well.
-      }
+            // TODO(vikrama) Need a test for SB as well.
+        }
 
-      /// <summary>
-      /// Put in checks for the length of the password for wep.
-      /// </summary>
-      [Test]
-      public void testWpa()
-      {
-         doTest("WIFI:S:TenChars;P:wow;T:WPA;;", "TenChars", "wow", "WPA");
-         doTest("WIFI:S:TenChars;P:space is silent;T:WPA;;", "TenChars", "space is silent", "WPA");
-         doTest("WIFI:S:TenChars;P:hellothere;T:WEP;;", "TenChars", "hellothere", "WEP");
+        /// <summary>
+        /// Put in checks for the length of the password for wep.
+        /// </summary>
+        [Test]
+        public void testWpa()
+        {
+            doTest("WIFI:S:TenChars;P:wow;T:WPA;;", "TenChars", "wow", "WPA");
+            doTest("WIFI:S:TenChars;P:space is silent;T:WPA;;", "TenChars", "space is silent", "WPA");
+            doTest("WIFI:S:TenChars;P:hellothere;T:WEP;;", "TenChars", "hellothere", "WEP");
 
-         // Escaped semicolons
-         doTest("WIFI:S:TenChars;P:hello\\;there;T:WEP;;", "TenChars", "hello;there", "WEP");
-         // Escaped colons
-         doTest("WIFI:S:TenChars;P:hello\\:there;T:WEP;;", "TenChars", "hello:there", "WEP");
-      }
+            // Escaped semicolons
+            doTest("WIFI:S:TenChars;P:hello\\;there;T:WEP;;", "TenChars", "hello;there", "WEP");
+            // Escaped colons
+            doTest("WIFI:S:TenChars;P:hello\\:there;T:WEP;;", "TenChars", "hello:there", "WEP");
+        }
 
-      [Test]
-      public void testEscape()
-      {
-         doTest("WIFI:T:WPA;S:test;P:my_password\\\\;;", "test", "my_password\\", "WPA");
-      }
+        [Test]
+        public void testEscape()
+        {
+            doTest("WIFI:T:WPA;S:test;P:my_password\\\\;;", "test", "my_password\\", "WPA");
+            doTest("WIFI:T:WPA;S:My_WiFi_SSID;P:abc123/;;", "My_WiFi_SSID", "abc123/", "WPA");
+        }
 
-      /// <summary>
-      /// Given the string contents for the barcode, check that it matches our expectations
-      /// </summary>
-      private static void doTest(String contents,
-                                 String ssid,
-                                 String password,
-                                 String type)
-      {
-         var fakeResult = new ZXing.Result(contents, null, null, BarcodeFormat.QR_CODE);
-         var result = ResultParser.parseResult(fakeResult);
+        /// <summary>
+        /// Given the string contents for the barcode, check that it matches our expectations
+        /// </summary>
+        private static void doTest(String contents,
+            String ssid,
+            String password,
+            String type)
+        {
+            var fakeResult = new ZXing.Result(contents, null, null, BarcodeFormat.QR_CODE);
+            var result = ResultParser.parseResult(fakeResult);
 
-         // Ensure it is a wifi code
-         Assert.AreEqual(ParsedResultType.WIFI, result.Type);
-         var wifiResult = (WifiParsedResult)result;
+            // Ensure it is a wifi code
+            Assert.AreEqual(ParsedResultType.WIFI, result.Type);
+            var wifiResult = (WifiParsedResult) result;
 
-         Assert.AreEqual(ssid, wifiResult.Ssid);
-         Assert.AreEqual(password, wifiResult.Password);
-         Assert.AreEqual(type, wifiResult.NetworkEncryption);
-      }
-   }
+            Assert.AreEqual(ssid, wifiResult.Ssid);
+            Assert.AreEqual(password, wifiResult.Password);
+            Assert.AreEqual(type, wifiResult.NetworkEncryption);
+        }
+    }
 }
