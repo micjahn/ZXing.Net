988cc1440e371a421d6e5c4d3556771237104c26
Michael <mikej06@hotmail.com>
2021-10-06 22:18:54 +0200

    SHA-1: f05963d63d74f9db4ea5451105a1692e7496d1d0
    * Update dependencies and add one new test

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -64,7 +64,7 @@ using System.Runtime.InteropServices;
 #if NETFX_CORE && !WINDOWS_UWP
 [assembly: AssemblyTitle("zxing.net for windows rt")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 20.02.2021 14:29:32)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 02.04.2021 18:59:49)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
