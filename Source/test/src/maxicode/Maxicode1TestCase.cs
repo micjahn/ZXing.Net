f82bba56b30a308c950493d6c8eae732fff80fa5
Michael <mikej06@hotmail.com>
2021-10-06 22:23:33 +0200

    port SHA-1: cad70351efe2cd732862c8b0bcd89f9dd23c4faa
    * MaxiCode decoding: add missing NS to Set C (#1375)

--- Source/test/src/maxicode/Maxicode1TestCase.cs
+++ Source/test/src/maxicode/Maxicode1TestCase.cs
@@ -26,7 +26,7 @@ namespace ZXing.Maxicode.Test
       public Maxicode1TestCase()
          : base("test/data/blackbox/maxicode-1", new MultiFormatReader(), BarcodeFormat.MAXICODE)
       {
-         addTest(5, 5, 0.0f);
+         addTest(6, 6, 0.0f);
       }
    }
 }
