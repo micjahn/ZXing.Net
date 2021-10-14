682fee5c99567cd2eeb44509e0ed8bd78cf61f98
Michael <mikej06@hotmail.com>
2021-10-14 06:50:50 +0200

    port SHA-1: 99e9b34f5afc21fdaeead283d5ed0bc1314cbec1
    * Update ResultMetadataType.java (#1376)

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -64,7 +64,7 @@ using System.Runtime.InteropServices;
 #if NETFX_CORE && !WINDOWS_UWP
 [assembly: AssemblyTitle("zxing.net for windows rt")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 04.04.2021 02:40:11)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 06.04.2021 17:19:57)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
