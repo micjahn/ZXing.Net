50a0a7b0a858ac437dd30dead99456ee715c2f24
Michael <mikej06@hotmail.com>
2023-02-16 22:55:55 +0100

    port SHA-1: 09e4a5824b8a2d64450e028869bf98e9a91a18bc
    * Fix javadoc warnings from last release

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -70,7 +70,7 @@ using System.Runtime.InteropServices;
 #if NETFX_CORE && !WINDOWS_UWP
 [assembly: AssemblyTitle("zxing.net for windows rt")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 27.04.2022 16:46:49)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 01.05.2022 18:51:35)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
