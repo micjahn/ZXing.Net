c94f40df433a13aa1a66bfb72a9a49f1f5a0dc82
Michael <mikej06@hotmail.com>
2023-02-18 15:17:18 +0100

    port SHA-1: 7fe6d3c1083a82c41d1f1e81b291a696fa259091
    * Possible fix for https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=50388

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Runtime.InteropServices;
 #if NETFX_CORE && !WINDOWS_UWP
 [assembly: AssemblyTitle("zxing.net for windows rt")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 20.07.2022 01:12:52)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 18.08.2022 15:42:06)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
