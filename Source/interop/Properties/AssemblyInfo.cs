6527d6b8398f76010fec14cfd8e9624d81aec3f2
Michael <mikej06@hotmail.com>
2023-02-21 22:38:01 +0100

    switching to 0.17

--- Source/interop/Properties/AssemblyInfo.cs
+++ Source/interop/Properties/AssemblyInfo.cs
@@ -75,8 +75,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.16.7.0")]
-[assembly: AssemblyFileVersion("0.16.7.0")]
+[assembly: AssemblyVersion("0.17.0.0")]
+[assembly: AssemblyFileVersion("0.17.0.0")]
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
