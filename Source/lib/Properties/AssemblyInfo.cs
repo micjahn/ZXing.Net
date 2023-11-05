4a6a0a2d57923b82f8743a6ac595988c3063254e
Michael <mikej06@hotmail.com>
2023-11-05 18:14:01 +0100

    port SHA-1: 465753afe637ee3ad3bd0710005934236e86143b
    * Avoid 2 more RSS Expanded finder pattern false positive scenarios (#1692)

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Runtime.InteropServices;
 #if NETFX_CORE && !WINDOWS_UWP
 [assembly: AssemblyTitle("zxing.net for windows rt")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 04.09.2023 16:00:50)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 15.09.2023 17:55:54)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
