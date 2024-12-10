42c770ea95631a23616244c657ddba9a953db978
Michael <mikej06@hotmail.com>
2024-12-10 21:39:21 +0100

    [#587] Upgrade targets to SkiaSharp 3.116.1

---
+++ Source/Bindings/ZXing.SkiaSharp.V2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("ZXing.Net Development")]
+[assembly: AssemblyProduct("ZXing.SkiaSharp")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyDescription("ZXing.Net Bindings to SkiaSharp")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e5df444d-d8b3-49d3-801b-59b6b1e90901")]
