cedd13075a14e5f3268083e643be641937c8dfa1
Michael <mikej06@hotmail.com>
2024-12-30 20:30:14 +0100

    port SHA-1: e750aca9e53d80eb7c2d7ffc3ccdb4e7cf6a7aca
    * Be more stringent on email validation rules (#1870)
    
    * be more stringent on email validation rules
    add UT to validate various email formats
    
    * fix indentation
    
    * fix failing UT case
    
    * refine regex for email's domain:
    * only letters in the last part
    * no part starting or ending with a - (- allowed inside the part)
    
    add UT to cover such cases

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Runtime.InteropServices;
 #if NETFX_CORE && !WINDOWS_UWP
 [assembly: AssemblyTitle("zxing.net for windows rt")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 02.08.2024 03:57:31)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 19.10.2024 03:38:56)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
