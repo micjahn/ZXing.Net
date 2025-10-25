f7dfb9ece3f62aa0e18482864b4a61de1ab26091
Michael <mikej06@hotmail.com>
2025-10-25 17:51:13 +0200

    port SHA-1: 6ae11863c7082cfeb62fa5075a488ec9588a01e6
    * Add hint to correct wrong dimension (#1901)
    
    * Add hint to correct wrong dimension
    
    Fix issue #1900
    
    * Revert "Add hint to correct wrong dimension"
    
    This reverts commit 16723b3434a78ab49894f611cfb57c3e712b9b97.
    
    * Correct wrong dimension to lower value
    
    Fix issue #1900
    
    * Cleanup code
    
    ---------
    
    Co-authored-by: Matthias Agethle <matthias.agethle@elca.ch>

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -58,7 +58,7 @@ using System.Runtime.InteropServices;
 #if NETFX_CORE && !WINDOWS_UWP
 [assembly: AssemblyTitle("zxing.net for windows rt")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 17.11.2024 17:25:04)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 04.02.2025 17:18:13)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
