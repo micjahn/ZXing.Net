8697ec1db43dea06dcbb5160f96b0f69963c32b7
Michael <mikej06@hotmail.com>
2025-10-25 17:52:39 +0200

    port SHA-1: 2a779a95863caebe2e72459bc3752287c2a96ead
    * Comment on distortion calculation for isosceles right triangles (#1908)
    
    * Fix distortion calculation for isosceles right triangles
    
    * Revert "Fix distortion calculation for isosceles right triangles"
    
    This reverts commit 4a8ad9a5bea9d0be251450f9e0db15b16e85e1ac.
    
    * Add comment to distortion calculation
    
    The formula for calculating the distortion seems wrong but practically it seems to work slightly better

--- Source/lib/Properties/AssemblyInfo.cs
+++ Source/lib/Properties/AssemblyInfo.cs
@@ -82,7 +82,7 @@ using System.Runtime.InteropServices;
 #if INTEROP
 [assembly: AssemblyTitle("zxing.net for com interop")]
 #endif
-[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 04.02.2025 17:18:13)")]
+[assembly: AssemblyDescription("port of the java based barcode scanning library for .net (java zxing 10.02.2025 15:22:04)")]
 [assembly: AssemblyCompany("ZXing.Net Development")]
 [assembly: AssemblyProduct("ZXing.Net")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
