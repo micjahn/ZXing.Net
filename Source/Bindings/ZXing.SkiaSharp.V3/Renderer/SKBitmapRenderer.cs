eb11f9706d25b991d663d54f2101f5527b091a41
Michael <mikej06@hotmail.com>
2026-07-06 20:39:27 +0200

    updated Skia Sharp Bindings

---
+++ Source/Bindings/ZXing.SkiaSharp.V3/Renderer/SKBitmapRenderer.cs
@@ -0,0 +1,260 @@
+﻿/*
+ * Copyright 2017 ZXing.Net authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Net.NetworkInformation;
+using SkiaSharp;
+
+using ZXing.Common;
+using ZXing.OneD;
+
+namespace ZXing.SkiaSharp.Rendering
+{
+    /// <summary>
+    /// Renders a <see cref="BitMatrix" /> to a <see cref="SKBitmap" /> image
+    /// </summary>
+    public class SKBitmapRenderer : ZXing.Rendering.IBarcodeRenderer<SKBitmap>
+    {
+        /// <summary>
+        /// Gets or sets the foreground color.
+        /// </summary>
+        /// <value>The foreground color.</value>
+        public SKColor Foreground { get; set; }
+
+        /// <summary>
+        /// Gets or sets the background color.
+        /// </summary>
+        /// <value>The background color.</value>
+        public SKColor Background { get; set; }
+
+        /// <summary>
+        /// Gets or sets the text font.
+        /// </summary>
+        /// <value>
+        /// The text font.
+        /// </value>
+        public SKTypeface TextFont { get; set; }
+
+        /// <summary>
+        /// Gets or sets the height of the text
+        /// </summary>
+        public float TextSize { get; set; }
+
+        private static readonly SKTypeface DefaultTextFont;
+
+        static SKBitmapRenderer()
+        {
+            try
+            {
+                DefaultTextFont = SKTypeface.FromFamilyName("Arial", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright);
+            }
+            catch (Exception)
+            {
+                // have to ignore, no better idea
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SKBitmapRenderer"/> class.
+        /// </summary>
+        public SKBitmapRenderer()
+        {
+            Foreground = SKColors.Black;
+            Background = SKColors.White;
+            TextFont = DefaultTextFont;
+            TextSize = 10;
+        }
+
+        /// <summary>
+        /// Renders the specified matrix.
+        /// </summary>
+        /// <param name="matrix">The matrix.</param>
+        /// <param name="format">The format.</param>
+        /// <param name="content">The content.</param>
+        /// <returns></returns>
+        public SKBitmap Render(BitMatrix matrix, BarcodeFormat format, string content)
+        {
+            return Render(matrix, format, content, new EncodingOptions());
+        }
+
+        /// <summary>
+        /// Renders the specified matrix.
+        /// </summary>
+        /// <param name="matrix">The matrix.</param>
+        /// <param name="format">The format.</param>
+        /// <param name="content">The content.</param>
+        /// <param name="options">The options.</param>
+        /// <returns></returns>
+        virtual public SKBitmap Render(BitMatrix matrix, BarcodeFormat format, string content, EncodingOptions options)
+        {
+            var width = matrix.Width;
+            var height = matrix.Height;
+            var font = TextFont ?? DefaultTextFont;
+            var emptyArea = 0;
+            var outputContent = font != null &&
+                                (options == null || !options.PureBarcode) &&
+                                !String.IsNullOrEmpty(content) &&
+                                (format == BarcodeFormat.CODE_39 ||
+                                 format == BarcodeFormat.CODE_93 ||
+                                 format == BarcodeFormat.CODE_128 ||
+                                 format == BarcodeFormat.EAN_13 ||
+                                 format == BarcodeFormat.EAN_8 ||
+                                 format == BarcodeFormat.CODABAR ||
+                                 format == BarcodeFormat.ITF ||
+                                 format == BarcodeFormat.UPC_A ||
+                                 format == BarcodeFormat.UPC_E ||
+                                 format == BarcodeFormat.MSI ||
+                                 format == BarcodeFormat.PLESSEY);
+
+            if (options != null && !options.NoPadding)
+            {
+                if (options.Width > width)
+                {
+                    width = options.Width;
+                }
+                if (options.Height > height)
+                {
+                    height = options.Height;
+                }
+            }
+
+            // calculating the scaling factor
+            var pixelsizeWidth = width / matrix.Width;
+            var pixelsizeHeight = height / matrix.Height;
+
+            SKBitmap bitmap = new SKBitmap(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
+            IntPtr pixelsAddr = bitmap.GetPixels();
+
+            unsafe
+            {
+                var ptr = (uint*) pixelsAddr.ToPointer();
+                var forecolor = (uint) Foreground;
+                var backcolor = (uint) Background;
+                var textAreaHeight = (int) (TextSize < 1 ? 10 : TextSize);
+
+                emptyArea = outputContent && height > textAreaHeight ? textAreaHeight : 0;
+
+                for (int y = 0; y < matrix.Height - emptyArea; y++)
+                {
+                    // stretching the line by the scaling factor
+                    for (var pixelsizeHeightProcessed = 0;
+                        pixelsizeHeightProcessed < pixelsizeHeight;
+                        pixelsizeHeightProcessed++)
+                    {
+                        // going through the columns of the current line
+                        for (var x = 0; x < matrix.Width; x++)
+                        {
+                            var color = matrix[x, y] ? forecolor : backcolor;
+                            // stretching the columns by the scaling factor
+                            for (var pixelsizeWidthProcessed = 0;
+                                pixelsizeWidthProcessed < pixelsizeWidth;
+                                pixelsizeWidthProcessed++)
+                            {
+                                *ptr++ = color;
+                            }
+                        }
+                        // fill up to the right if the barcode doesn't fully fit in
+                        for (var x = pixelsizeWidth * matrix.Width; x < width; x++)
+                        {
+                            *ptr++ = backcolor;
+                        }
+                    }
+                }
+                // fill up to the bottom if the barcode doesn't fully fit in
+                for (var y = pixelsizeHeight * matrix.Height; y < height; y++)
+                {
+                    for (var x = 0; x < width; x++)
+                    {
+                        *ptr++ = backcolor;
+                    }
+                }
+                // fill the bottom area with the background color if the content should be written below the barcode
+                if (outputContent)
+                {
+                    if (emptyArea > 0)
+                    {
+                        for (int y = height - emptyArea; y < height; y++)
+                        {
+                            for (var x = 0; x < width; x++)
+                            {
+                                *ptr++ = backcolor;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (emptyArea > 0)
+            {
+                //using (var surface = SKSurface.Create(width, height, SKImageInfo.PlatformColorType, SKAlphaType.Premul))
+                using (SKCanvas myCanvas = new SKCanvas(bitmap))
+                using (var paint = new SKPaint())
+                using (var fontForPaint = new SKFont())
+                {
+                    paint.IsAntialias = true;
+                    paint.Color = Foreground;
+                    fontForPaint.Typeface = font;
+                    fontForPaint.Size = TextSize < 1 ? 10 : TextSize;
+
+                    // output content text below the barcode
+                    if (emptyArea > 0)
+                    {
+                        switch (format)
+                        {
+                            case BarcodeFormat.UPC_E:
+                            case BarcodeFormat.EAN_8:
+                                if (content.Length < 8)
+                                    content = OneDimensionalCodeWriter.CalculateChecksumDigitModulo10(content);
+                                if (content.Length > 4)
+                                    content = content.Insert(4, "   ");
+                                break;
+                            case BarcodeFormat.EAN_13:
+                                if (content.Length < 13)
+                                    content = OneDimensionalCodeWriter.CalculateChecksumDigitModulo10(content);
+                                if (content.Length > 7)
+                                    content = content.Insert(7, "   ");
+                                if (content.Length > 1)
+                                    content = content.Insert(1, "   ");
+                                break;
+                            case BarcodeFormat.UPC_A:
+                                if (content.Length < 12)
+                                    content = OneDimensionalCodeWriter.CalculateChecksumDigitModulo10(content);
+                                if (content.Length > 11)
+                                    content = content.Insert(11, "   ");
+                                if (content.Length > 6)
+                                    content = content.Insert(6, "   ");
+                                if (content.Length > 1)
+                                    content = content.Insert(1, "   ");
+                                break;
+                        }
+#if SKIASHARP_V2
+                        var textWidth = paint.MeasureText(content);
+#else
+                        var textWidth = fontForPaint.MeasureText(content, paint);
+#endif
+                        var x = (pixelsizeWidth * matrix.Width - textWidth) / 2;
+                        var y = height - 1;
+                        x = x < 0 ? 0 : x;
+                        myCanvas.DrawText(content, x, y, fontForPaint, paint);
+                    }
+                    myCanvas.Flush();
+                }
+            }
+
+            return bitmap;
+        }
+    }
+}
---
+++ Source/Bindings/ZXing.SkiaSharp.V3/SKBitmapLuminanceSource.cs
@@ -0,0 +1,141 @@
+﻿/*
+ * Copyright 2017 ZXing.Net authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Runtime.CompilerServices;
+using SkiaSharp;
+
+namespace ZXing.SkiaSharp
+{
+    /// <summary>
+    /// A luminance source class which consumes a Mat image from SkiaSharp and calculates the luminance values based on the bytes of the image
+    /// </summary>
+    public class SKBitmapLuminanceSource : BaseLuminanceSource
+    {
+        /// <summary>
+        /// initializing constructor
+        /// </summary>
+        /// <param name="image"></param>
+        public SKBitmapLuminanceSource(SKBitmap image)
+           : base(image.Width, image.Height)
+        {
+            CalculateLuminance(image);
+        }
+
+        /// <summary>
+        /// internal constructor used by CreateLuminanceSource
+        /// </summary>
+        /// <param name="luminances"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        protected SKBitmapLuminanceSource(byte[] luminances, int width, int height)
+           : base(luminances, width, height)
+        {
+        }
+
+        /// <summary>
+        /// Should create a new luminance source with the right class type.
+        /// The method is used in methods crop and rotate.
+        /// </summary>
+        /// <param name="newLuminances">The new luminances.</param>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        /// <returns></returns>
+        protected override LuminanceSource CreateLuminanceSource(byte[] newLuminances, int width, int height)
+        {
+            return new SKBitmapLuminanceSource(newLuminances, width, height);
+        }
+
+        private void CalculateLuminance(SKBitmap src)
+        {
+            if (src == null)
+                throw new ArgumentNullException("src");
+
+            var imageInfo = src.Info;
+            var width = imageInfo.Width;
+            var height = imageInfo.Height;
+            var colorType = imageInfo.ColorType;
+
+#if SKColorType_Index8_Support
+            if (colorType == SKColorType.Index8)
+            {
+                // use the color table for indexed images
+                for (int row = 0; row < height; row++)
+                {
+                    var index = width * row;
+                    for (int col = 0; col < width; col++, index++)
+                    {
+                        var pixel = src.GetIndex8Color(col, row);
+
+                        SetLuminance(index, pixel.Red, pixel.Green, pixel.Blue, pixel.Alpha);
+                    }
+                }
+            }
+            else
+#endif
+            if (colorType == SKColorType.Rgba8888
+                    || colorType == SKColorType.RgbaF16
+                    || colorType == SKColorType.Bgra8888)
+            {
+                // Read pixels from unmanaged memory to set luminance
+                // https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/graphics/skiasharp/bitmaps/pixel-bits
+                IntPtr pixelsAddr = src.GetPixels();
+
+                unsafe
+                {
+                    uint* ptr = (uint*)pixelsAddr.ToPointer();
+
+                    for (int row = 0; row < height; row++)
+                    {
+                        var index = width * row;
+                        for (int col = 0; col < width; col++, index++)
+                        {
+                            uint colorVal = *ptr++;
+
+                            var alpha = (byte)((colorVal >> 24) & 0xff);
+                            var red = (byte)((colorVal >> 16) & 0xff);
+                            var green = (byte)((colorVal >> 8) & 0xff);
+                            var blue = (byte)((colorVal) & 0xff);
+
+                            SetLuminance(index, red, green, blue, alpha);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // unknown type or other color types, use the 'old' way allocating more managed memory
+                // can potentially migrate this to the above case, but leaving here for safety
+                var pixels = src.Pixels;
+                var length = pixels.Length;
+                for (var index = 0; index < length; index++)
+                {
+                    var pixel = pixels[index];
+
+                    SetLuminance(index, pixel.Red, pixel.Green, pixel.Blue, pixel.Alpha);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void SetLuminance(int index, byte red, byte green, byte blue, byte alpha)
+        {
+            // Calculate luminance cheaply, favoring green.
+            var luminance = (byte)((RChannelWeight * red + GChannelWeight * green + BChannelWeight * blue) >> ChannelWeight);
+            luminances[index] = (byte)(((luminance * alpha) >> 8) + (255 * (255 - alpha) >> 8));
+        }
+    }
+}
